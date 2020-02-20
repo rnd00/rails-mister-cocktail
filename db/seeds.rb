272076f6c130e782c5c74479c5053a7906f9656b
Ryanda Diedan <rndmx00@gmail.com>
2020-02-20 18:39:07 +0900

    seeds init

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+# clearance
+puts 'Cleanup...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+# fetch the indegrients
+puts "Fetching #{url}..."
+raw_data = open(url).read
+# parse and convert to a string
+puts 'Converting raw data to an array...'
+raw_arr = JSON.parse(raw_data)
+ingredients = raw_arr['drinks'].map { |ing| ing['strIngredient1'] }
+# put into db
+puts 'Seeding the tables...'
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient)
+end
