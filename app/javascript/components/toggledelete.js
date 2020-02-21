c343542048b61d7008946a578f827d19d62c8e1e
Ryanda Diedan <rndmx00@gmail.com>
2020-02-21 15:59:07 +0900

    delete button for cocktails

--- app/javascript/components/toggledelete.js
+++ app/javascript/components/toggledelete.js
@@ -2,7 +2,7 @@ const toggleDelete = () => {
   const b = document.getElementById('toggle-delete');
   if (b) {
     b.addEventListener("click", () => {
-      const a = document.querySelectorAll('#delete-ingredient');
+      const a = document.querySelectorAll('#delete-button');
       a.forEach( e => e.classList.toggle("hidden"));
     });
   }
